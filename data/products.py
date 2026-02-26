b083bcaed376a74ab430e9b60fd0eb63bed629bf
lohojda520-source <lohojda520@gmail.com>
2026-02-26 14:04:37 +0200

    products.py

---
+++ data/products.py
@@ -0,0 +1,12 @@
+PRODUCTS = {
+    "google": {
+        "name": "Google Ads System",
+        "file": "assets/google_ads.pdf",
+        "price": "49"
+    },
+    "meta": {
+        "name": "Meta Ads System",
+        "file": "assets/meta_ads.pdf",
+        "price": "79"
+    }
+}
