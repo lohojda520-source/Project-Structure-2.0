f14faebe58b47671cca59ff34ca7cce3f24686ba
lohojda520-source <lohojda520@gmail.com>
2026-02-26 17:39:59 +0200

    tart

--- handlers/start.py
+++ handlers/start.py
@@ -1,10 +1,1 @@
-from aiogram import Router
-from aiogram.filters import Command
-from aiogram.types import Message
-from handlers.products import show_products  # функція з products.py
 
-router = Router()
-
-@router.message(Command("start"))
-async def start_handler(message: Message):
-    await show_products(message)
