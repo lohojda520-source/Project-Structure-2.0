d5b026be3d6c9b960c973e1bab799e809824c64b
lohojda520-source <lohojda520@gmail.com>
2026-02-26 16:47:36 +0200

    config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
-
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 print("BOT_TOKEN:", BOT_TOKEN)
 PAYPAL_CLIENT_ID = os.environ.get("PAYPAL_CLIENT_ID")
 PAYPAL_SECRET = os.environ.get("PAYPAL_SECRET")
 PAYPAL_BASE = os.environ.get("PAYPAL_BASE")
 APP_BASE_URL = os.environ.get("APP_BASE_URL")
+
 
