1681ad50478632c4cee58ef0cf381feb61ce1ed0
lohojda520-source <lohojda520@gmail.com>
2026-02-26 17:16:46 +0200

    start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,9 +1,10 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
+from handlers.products import show_products  # функція з products.py
 
 router = Router()
 
 @router.message(Command("start"))
 async def start_handler(message: Message):
-    await message.answer("Use /products to see available packages.")
+    await show_products(message)
